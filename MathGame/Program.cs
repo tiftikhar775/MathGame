eab878e2603031ed9d92979d6333aeeb46e68e41
tifti <tifti@DESKTOP-U8QO9HP>
2024-05-02 18:12:50 -0500

    added menu options and methods for mathematical operations

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -4,9 +4,73 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name);
+            DisplayMenu();
+        }
+
+        static void DisplayMenu()
+        {
+            Console.WriteLine("Choose Mode:\n");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+
+            if (option == 1)
+            {
+                AdditionMode();
+            } 
+            else if (option == 2)
+            {
+                SubtractionMode();
+            } 
+            else if (option == 3)
+            {
+                MultiplicationMode();
+            } 
+            else if (option==4)
+            {
+                DivisionMode();
+            }
+            
+
+        }
+
+        static void AdditionMode()
+        {
+            Random randomAdd = new Random(); // random class new instance
+            int num1 = randomAdd.Next(99);
+            int num2 = randomAdd.Next(99);
+            Console.WriteLine($"{num1} + {num2}");
+
+        }
+
+        static void SubtractionMode()
+        {
+            Random randomSub = new Random(); // random class new instance
+            int num1 = randomSub.Next(99);
+            int num2 = randomSub.Next(99);
+            Console.WriteLine($"{num1} - {num2}");
+        }
+
+        static void MultiplicationMode()
+        {
+            Random randomMult = new Random(); // random class new instance
+            int num1 = randomMult.Next(99);
+            int num2 = randomMult.Next(99);
+            Console.WriteLine($"{num1} * {num2}");
+        }
+
+        static void DivisionMode()
+        {
+            Random randomDiv = new Random(); // random class new instance
+            int num1 = randomDiv.Next(99);
+            int num2 = randomDiv.Next(99);
+            Console.WriteLine($"{num1} / {num2}");
+
         }
     }
 }
