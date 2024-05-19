500adb5f56e0eea4cc62cc57b84d63b9e2b74eda
tifti <tifti@DESKTOP-U8QO9HP>
2024-05-19 00:38:40 -0500

    Reformatted code to make it more readable.

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -14,7 +14,6 @@
             while (true)
             {
                 //Console.WriteLine(difficulty);
-
                 Console.WriteLine("Choose Mode:\n");
                 Console.WriteLine("1. Addition");
                 Console.WriteLine("2. Subtraction");
@@ -23,72 +22,28 @@
                 Console.WriteLine("5. Change Difficulty");
                 Console.WriteLine("6. Quit\n");
 
-                Console.Write("Enter here: ");
-                int option = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Select Mode: ");
+                int mode = Convert.ToInt32(Console.ReadLine());
 
-                switch (option)
+                if (mode == 5)
                 {
-                    case 1:
-                        AdditionMode(difficulty);
-                        continue;
-
-                    case 2:
-                        SubtractionMode(difficulty);
-                        continue;
-
-                    case 3:
-                        MultiplicationMode(difficulty);
-                        continue;
-
-                    case 4:
-                        DivisionMode(difficulty);
-                        continue;  
-
-                    case 5: 
-                        difficulty = DifficultySelect(); // reassign difficulty value
-                        continue;
-
-                    case 6:
-                        Console.WriteLine("Goodbye...");
-                        break;
-
-                    default:
-                        Console.WriteLine("Please enter a valid option");
-                        continue;
-                }
-                break;
-
-/*
-                if (option == 1)
-                {
-                    AdditionMode(difficulty);
-                }
-                else if (option == 2)
-                {
-                    SubtractionMode();
-                }
-                else if (option == 3)
-                {
-                    MultiplicationMode();
-                }
-                else if (option == 4)
-                {
-                    DivisionMode();
-                } 
-                else if (option == 5) 
-                { 
+                    difficulty = DifficultySelect();
                     continue;
                 }
-                else if (option == 6)
+
+                // quit game
+                if (mode == 6)
                 {
                     break;
-                }*/
-            }
+                }
 
-           
+                MathModeSelect(difficulty, mode);
+
+            }
         }
 
-        static int DifficultySelect() {
+        static int DifficultySelect()
+        {
             Console.WriteLine("Choose Difficulty:\n");
             Console.WriteLine("1. Easy");
             Console.WriteLine("2. Medium");
@@ -98,402 +53,134 @@
             {
                 Console.Write("Enter here: ");
                 difficulty = Convert.ToInt32(Console.ReadLine());
-            } while (difficulty < 0 || difficulty > 3);
+            } while (difficulty < 1 || difficulty > 3);
 
             return difficulty;
         }
 
-        static void AdditionMode(int difficulty)
+        static int GenerateRandomNumber(int difficulty)
         {
-            int attempts = 3;
-            int score = 0;
+            int number = 0;
 
-            // game loop
-            while (attempts > 0) {
-                Random randomAdd = new Random(); // random class new instance
-                int num1, num2, total, answer; 
-
-                // change number range depending on difficulty
-                // easy difficulty
-                if (difficulty == 1)
-                {
-                    num1 = randomAdd.Next(99);
-                    num2 = randomAdd.Next(99);
-                    total = num1 + num2;
-
-                    Console.WriteLine($"{num1} + {num2} = ??");
-
-                    answer = Convert.ToInt32(Console.ReadLine());
-
-                    if (answer == total)
-                    {
-                        Console.WriteLine("Correct!!");
-                        score++;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Wrong. Correct Answer is: {total}");
-                        attempts--;
-                        if (score > 0) // prevents negative score
-                        {
-                            score--;
-                        }
-                    }
-
-                }
-                // medium difficulty
-                if (difficulty == 2)
-                {
-                    num1 = randomAdd.Next(999);
-                    num2 = randomAdd.Next(999);
-                    total = num1 + num2;
-
-                    Console.WriteLine($"{num1} + {num2} = ??");
-
-                    answer = Convert.ToInt32(Console.ReadLine());
-
-                    if (answer == total)
-                    {
-                        Console.WriteLine("Correct!!");
-                        score++;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Wrong. Correct Answer is: {total}");
-                        attempts--;
-                        if (score > 0) // prevents negative score
-                        {
-                            score--;
-                        }
-                    }
-                }
-                // hard difficulty
-                if (difficulty == 3)
-                {
-                    num1 = randomAdd.Next(9999);
-                    num2 = randomAdd.Next(9999);
-                    total = num1 + num2;
-
-                    Console.WriteLine($"{num1} + {num2} = ??");
-
-                    answer = Convert.ToInt32(Console.ReadLine());
-
-                    if (answer == total)
-                    {
-                        Console.WriteLine("Correct!!");
-                        score++;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Wrong. Correct Answer is: {total}");
-                        attempts--;
-                        if (score > 0) // prevents negative score
-                        {
-                            score--;
-                        }
-                    }
-                }
-
-                
+            Random random = new Random();
+            
+            if (difficulty == 1)
+            {
+                number = random.Next(0, 50);
+            } 
+            else if (difficulty == 2)
+            {
+                number = random.Next(0, 99);
+            } 
+            else if(difficulty == 3)
+            {
+                number = random.Next(0, 999);
             }
 
-            Console.WriteLine($"Game over. Your score was {score} points");
+            return number;
 
         }
-
-        static void SubtractionMode(int difficulty)
+        
+        static void MathModeSelect(int difficulty, int mode)
         {
             int attempts = 3;
             int score = 0;
+            int num1, num2, answer, correctAnswer;
 
-            // game loop
             while (attempts > 0)
             {
-                Random randomAdd = new Random(); // random class new instance
-                int num1, num2, total, answer;
-
-                // change number range depending on difficulty
-                // easy difficulty
-                if (difficulty == 1)
+                if (mode == 1)
                 {
-                    num1 = randomAdd.Next(99);
-                    num2 = randomAdd.Next(99);
-                    total = num1 - num2;
-
-                    Console.WriteLine($"{num1} - {num2} = ??");
+                    num1 = GenerateRandomNumber(difficulty);
+                    num2 = GenerateRandomNumber(difficulty);
+                    correctAnswer = num1 + num2;
+                    Console.WriteLine($"{num1} + {num2} == ??");
 
                     answer = Convert.ToInt32(Console.ReadLine());
 
-                    if (answer == total)
+                    if (answer == correctAnswer)
                     {
-                        Console.WriteLine("Correct!!");
+                        Console.WriteLine("Correct!");
+                        score++;
+                    } 
+                    else
+                    {
+                        Console.WriteLine($"Wrong. Correct Answer is: {correctAnswer}");
+                        attempts--;
+                    }
+
+
+                }
+
+                if (mode == 2)
+                {
+                    num1 = GenerateRandomNumber(difficulty);
+                    num2 = GenerateRandomNumber(difficulty);
+                    correctAnswer = num1 - num2;
+                    Console.WriteLine($"{num1} - {num2} == ??");
+
+                    answer = Convert.ToInt32(Console.ReadLine());
+
+                    if (answer == correctAnswer)
+                    {
+                        Console.WriteLine("Correct!");
                         score++;
                     }
                     else
                     {
-                        Console.WriteLine($"Wrong. Correct Answer is: {total}");
+                        Console.WriteLine($"Wrong. Correct Answer is: {correctAnswer}");
                         attempts--;
-                        if (score > 0) // prevents negative score
-                        {
-                            score--;
-                        }
                     }
 
-                }
-                // medium difficulty
-                if (difficulty == 2)
-                {
-                    num1 = randomAdd.Next(999);
-                    num2 = randomAdd.Next(999);
-                    total = num1 - num2;
 
-                    Console.WriteLine($"{num1} - {num2} = ??");
+                }
+
+                if (mode == 3)
+                {
+                    num1 = GenerateRandomNumber(difficulty);
+                    num2 = GenerateRandomNumber(difficulty);
+                    correctAnswer = num1 * num2;
+                    Console.WriteLine($"{num1} x {num2} == ??");
 
                     answer = Convert.ToInt32(Console.ReadLine());
 
-                    if (answer == total)
+                    if (answer == correctAnswer)
                     {
-                        Console.WriteLine("Correct!!");
+                        Console.WriteLine("Correct!");
                         score++;
                     }
                     else
                     {
-                        Console.WriteLine($"Wrong. Correct Answer is: {total}");
+                        Console.WriteLine($"Wrong. Correct Answer is: {correctAnswer}");
                         attempts--;
-                        if (score > 0) // prevents negative score
-                        {
-                            score--;
-                        }
                     }
-                }
-                // hard difficulty
-                if (difficulty == 3)
-                {
-                    num1 = randomAdd.Next(9999);
-                    num2 = randomAdd.Next(9999);
-                    total = num1 - num2;
 
-                    Console.WriteLine($"{num1} - {num2} = ??");
+
+                }
+
+                if (mode == 4)
+                {
+                    num1 = GenerateRandomNumber(difficulty);
+                    num2 = GenerateRandomNumber(difficulty);
+                    correctAnswer = num1 / num2;
+                    Console.WriteLine($"{num1} / {num2} == ??");
 
                     answer = Convert.ToInt32(Console.ReadLine());
 
-                    if (answer == total)
+                    if (answer == correctAnswer)
                     {
-                        Console.WriteLine("Correct!!");
+                        Console.WriteLine("Correct!");
                         score++;
                     }
                     else
                     {
-                        Console.WriteLine($"Wrong. Correct Answer is: {total}");
+                        Console.WriteLine($"Wrong. Correct Answer is: {correctAnswer}");
                         attempts--;
-                        if (score > 0) // prevents negative score
-                        {
-                            score--;
-                        }
                     }
                 }
-
-
             }
 
-            Console.WriteLine($"Game over. Your score was {score} points");
         }
-
-        static void MultiplicationMode(int difficulty)
-        {
-            int attempts = 3;
-            int score = 0;
-
-            // game loop
-            while (attempts > 0)
-            {
-                Random randomAdd = new Random(); // random class new instance
-                int num1, num2, total, answer;
-
-                // change number range depending on difficulty
-                // easy difficulty
-                if (difficulty == 1)
-                {
-                    num1 = randomAdd.Next(99);
-                    num2 = randomAdd.Next(99);
-                    total = num1 * num2;
-
-                    Console.WriteLine($"{num1} x {num2} = ??");
-
-                    answer = Convert.ToInt32(Console.ReadLine());
-
-                    if (answer == total)
-                    {
-                        Console.WriteLine("Correct!!");
-                        score++;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Wrong. Correct Answer is: {total}");
-                        attempts--;
-                        if (score > 0) // prevents negative score
-                        {
-                            score--;
-                        }
-                    }
-
-                }
-                // medium difficulty
-                if (difficulty == 2)
-                {
-                    num1 = randomAdd.Next(999);
-                    num2 = randomAdd.Next(999);
-                    total = num1 * num2;
-
-                    Console.WriteLine($"{num1} x {num2} = ??");
-
-                    answer = Convert.ToInt32(Console.ReadLine());
-
-                    if (answer == total)
-                    {
-                        Console.WriteLine("Correct!!");
-                        score++;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Wrong. Correct Answer is: {total}");
-                        attempts--;
-                        if (score > 0) // prevents negative score
-                        {
-                            score--;
-                        }
-                    }
-                }
-                // hard difficulty
-                if (difficulty == 3)
-                {
-                    num1 = randomAdd.Next(9999);
-                    num2 = randomAdd.Next(9999);
-                    total = num1 * num2;
-
-                    Console.WriteLine($"{num1} x {num2} = ??");
-
-                    answer = Convert.ToInt32(Console.ReadLine());
-
-                    if (answer == total)
-                    {
-                        Console.WriteLine("Correct!!");
-                        score++;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Wrong. Correct Answer is: {total}");
-                        attempts--;
-                        if (score > 0) // prevents negative score
-                        {
-                            score--;
-                        }
-                    }
-                }
-
-
-            }
-
-            Console.WriteLine($"Game over. Your score was {score} points");
-        }
-
-        static void DivisionMode(int difficulty)
-        {
-            int attempts = 3;
-            int score = 0;
-
-            // game loop
-            while (attempts > 0)
-            {
-                Random randomAdd = new Random(); // random class new instance
-                int num1, num2, total, answer;
-
-                // change number range depending on difficulty
-                // easy difficulty
-                if (difficulty == 1)
-                {
-                    num1 = randomAdd.Next(99);
-                    num2 = randomAdd.Next(99);
-                    total = num1 / num2;
-
-                    Console.WriteLine($"{num1} / {num2} = ??");
-
-                    answer = Convert.ToInt32(Console.ReadLine());
-
-                    if (answer == total)
-                    {
-                        Console.WriteLine("Correct!!");
-                        score++;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Wrong. Correct Answer is: {total}");
-                        attempts--;
-                        if (score > 0) // prevents negative score
-                        {
-                            score--;
-                        }
-                    }
-
-                }
-                // medium difficulty
-                if (difficulty == 2)
-                {
-                    num1 = randomAdd.Next(999);
-                    num2 = randomAdd.Next(999);
-                    total = num1 + num2;
-
-                    Console.WriteLine($"{num1} / {num2} = ??");
-
-                    answer = Convert.ToInt32(Console.ReadLine());
-
-                    if (answer == total)
-                    {
-                        Console.WriteLine("Correct!!");
-                        score++;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Wrong. Correct Answer is: {total}");
-                        attempts--;
-                        if (score > 0) // prevents negative score
-                        {
-                            score--;
-                        }
-                    }
-                }
-                // hard difficulty
-                if (difficulty == 3)
-                {
-                    num1 = randomAdd.Next(9999);
-                    num2 = randomAdd.Next(9999);
-                    total = num1 / num2;
-
-                    Console.WriteLine($"{num1} / {num2} = ??");
-
-                    answer = Convert.ToInt32(Console.ReadLine());
-
-                    if (answer == total)
-                    {
-                        Console.WriteLine("Correct!!");
-                        score++;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Wrong. Correct Answer is: {total}");
-                        attempts--;
-                        if (score > 0) // prevents negative score
-                        {
-                            score--;
-                        }
-                    }
-                }
-
-
-            }
-
-            Console.WriteLine($"Game over. Your score was {score} points");
-
-        }
+       
     }
 }
